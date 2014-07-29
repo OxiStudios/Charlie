f4035d3860d58bca8054f00a60c9234d6d311f4b
butlernc <butlernc@uwec.edu>
2014-07-29 15:38:30 -0500

    added some to the level classes

--- core/src/com/oxistudios/charlie/levels/MasterLevel.java
+++ core/src/com/oxistudios/charlie/levels/MasterLevel.java
@@ -1,11 +1,18 @@
 package com.oxistudios.charlie.levels;
 
+
 public class MasterLevel {
 	
+	
 	public MasterLevel() {
+	}
+	
+	public void readHeader() {
 		
 	}
 	
-	
+	public void readData() {
+		
+	}
 
 }
