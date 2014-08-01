b9d65aa9444d99d1e8a7339133239bddb2235268
liddictm <liddictm@uwec.edu>
2014-08-01 10:44:00 -0500

    MasterMusic class created and implemented. More to add once I figure out TextureAtlas stuff.

---
+++ core/src/com/oxistudios/charlie/sound_engine/MasterMusic.java
@@ -0,0 +1,57 @@
+
+public class MasterMusic {
+	private SavingController saving_controller;
+	
+	private String sound_location;
+	private String[] sound_data;
+	private int place;
+	
+	public MasterMusic(SavingController saving_controller, String sound_location){
+		this.saving_controller = saving_controller;
+		this.sound_location = sound_location;
+		
+		place = 0;
+	}
+	public void readSound(){
+		sound_data = saving_controller.read(sound_location);
+	}
+	public Array<String> getSoundHeaderBlock(){
+		Array<String> soundHeader = new Array<String>();
+		
+		for(String data : sound_data){
+			if(data != "END_SOUND_HEADER"){
+				soundHeader.add(data);
+			}else{
+				break;
+			}
+			place++;
+		}
+		return soundHeader;
+	}
+	public Array<String> getSoundDataBlock(){
+		Array<String> data = new Array<String>();
+		
+		for(String single_data : sound_data){
+			place--;
+			if(place != 0 && single_data != "END_DATA"){
+				data.add(single_data);
+			}else{
+				break;
+			}
+		}
+		return data;
+	}
+	public TextureAtlas getTextureAtlas(Array<String> soundHeader){
+		for(int i = 0; i < soundHeader.size; i++){
+			if(soundHeader.get(i) == "TEXTURE_ATLAS_LOCATION"){
+				return new TextureAtlas(Gdx.files.external(header.get(i + 1)));
+			}
+		}
+		return null;
+	}
+	public HashMap<Object, Integer> loadSounds(Array<String> soundHeader, TextureAtlas texture_atlas){
+		 
+	}
+	
+	
+}
