0e539a872fffe84a7156da9cce5c55f1a8211ad6
Tyler Liddicoat <liddictm@uwec.edu>
2014-08-05 14:13:11 -0500

    Light stuff updated
    started the light stuff

--- core/src/com/oxistudios/charlie/lighting_engine/LightController.java
+++ core/src/com/oxistudios/charlie/lighting_engine/LightController.java
@@ -1,5 +1,39 @@
 package com.oxistudios.charlie.lighting_engine;
 
 public class LightController {
-
+	static final Color defaultColor = new Color(0.75f, 0.75f, 0.5f, 0.75f);
+	private boolean activeLight = true;
+	private boolean softLight = true;
+	private boolean xrayLight = false;
+	protected boolean staticLight = false;
+	protected float softShadowLength = 2.5f;
+	protected RayHandler rayHandler;
+	protected boolean culled = false;
+	protected int rayNumber;
+	protected int vertexNumber;
+	protected float distance;
+	protected float direction;
+	
+	protected Color color = new Color();
+	protected Mesh lightMesh;
+	protected Mesh softShadowMesh;
+	protected float colorF;
+	
+	final static int miniumum_rays = 3;
+	
+	float segements[];
+	float[] mx;
+	float[] my;
+	float[] f;
+	int m_index = 0;
+	
+	public LightController(RayHandler rayHandler, int rays, Color color, float directionDegree, float distance){
+		rayHandler.lightList.add(this);
+		this.rayHandler = rayHandler;
+		setRayNum(rays);
+		this.direction = directionDegree;
+		distance *= RayHandler.gammaCorrectionParameter;
+		this.distance = distance < 0.01f ? 0.01f : distance;
+		setColor(color);
+	}
 }
