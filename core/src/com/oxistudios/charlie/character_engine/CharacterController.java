b4ebda239a9cfc0ddbd156f4bd37ef8fd93481e2
Noah Butler <butlernc@uwec.edu>
2014-08-05 15:02:17 -0500

    enemies will have spawn locations on the map
    the character controller will handle the enemy spawners, the spawners
    will know where the camera is so if the person is getting close to a
    spawner it will create enemies that will go after charlie

--- core/src/com/oxistudios/charlie/character_engine/CharacterController.java
+++ core/src/com/oxistudios/charlie/character_engine/CharacterController.java
@@ -1,5 +1,44 @@
 package com.oxistudios.charlie.character_engine;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.oxistudios.charlie.level_engine.LevelController;
+
 public class CharacterController {
+	
+	LevelController level_controller;
+	Array<Entity> enemies;
+	
+	public CharacterController(LevelController level_controller) {
+		this.level_controller = level_controller;
+		
+		enemies = new Array<Entity>();
+		//fix enemy data in level controller
+		createEnemies(level_controller.getEnemy_data());
+	}
+	
+	public void createEnemies(Array<String> all_enemy_data) {
+		for(String Enemy_data : all_enemy_data) {
+			switch(Enemy_data) {
+				case "00-000":
+					//enemies.add(new FirstEnemy());
+					break;
+				case "00-001":
+					//enemies.add(new SecondEnemy());
+					break;
+			}
+		}
+	}
+	
+	//not checking if they are in view yet
+	public void render(float delta, SpriteBatch b) {
+		for(Entity enemy: enemies) {
+			enemy.render(delta, b);
+		}
+	}
+	
+	public void update(float delta) {
+		
+	}
 
 }
---
+++ core/src/com/oxistudios/charlie/character_engine/EnemySpawn.java
@@ -0,0 +1,15 @@
+package com.oxistudios.charlie.character_engine;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class EnemySpawn {
+	
+	private Vector2 position;
+	
+	public EnemySpawn(Vector2 position) {
+		this.position = position;
+	}
+	
+	
+
+}
