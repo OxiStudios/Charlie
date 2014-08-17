effdcfa27476f256ba82d5761d65015c2ce3bb03
Noah <butlernc@uwec.edu>
2014-08-17 17:25:26 -0500

    added a json reader and writer methods

--- core/src/com/oxistudios/charlie/saving_engine/SavingController.java
+++ core/src/com/oxistudios/charlie/saving_engine/SavingController.java
@@ -3,22 +3,26 @@ package com.oxistudios.charlie.saving_engine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Json;
 
 public class SavingController {
 	
+	private Json json;
+	private FileHandle file;
+	
 	public SavingController() {
-		
+		json = new Json();
 	}
 	
 	public String[] read(String location){
-		FileHandle file = Gdx.files.external(location);
+		file = Gdx.files.external(location);
 		String full = file.readString();
 		return full.split(",");
 		
 	}
 	
 	public void write(Array<String> dictionary, String location, boolean append) {
-		FileHandle file = Gdx.files.local(location);
+		file = Gdx.files.local(location);
 		for(String data: dictionary) {
 			file.writeString(data, append);
 		}
@@ -26,6 +30,22 @@ public class SavingController {
 	
 	public void save() {
 		
+	}
+	
+	public <T> Object create_object_json(Class<T> obj, String location) {
+		
+		
+		file = Gdx.files.external(location);
+		String raw_data = file.readString();
+		Object o = json.fromJson(obj, raw_data);
+		
+		return o;
+	}
+	
+	public void save_object_json(Object obj, String location) {
+		String raw_data = json.toJson(obj);
+		file = Gdx.files.external(location);
+		file.writeString(raw_data, false);
 	}
 	
 }
